dfac0e15066bc98984c778ace20a541304b2399c
Sycobuga <97840977+Sycobuga@users.noreply.github.com>
2022-06-10 15:42:38 +0530

    Update Bypass.py

--- Bypass.py
+++ Bypass.py
@@ -1,2 +1,26 @@
-from AKING import Fb
-Fb()
+import os, platform
+
+try:
+
+   import requests
+
+except:
+
+   os.system('pip install requests')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from axi import ahmad
+
+    ahmad()
+
+elif bit == '32bit':
+
+    from axi import ahmad
+
+    ahmad()
+
